2379ab2d9936c63f580264d496bb89d956f00dcd
Naouali <elnaoualinebil@gmail.com>
2020-05-14 00:44:04 +0100

    if not min

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -22,14 +22,17 @@ void selection_sort(int *array, size_t size)
 	min = i;
 	for (j = i + 1; j < size; j++)
     {
-	if (array[j] < array[min])
+	      if (array[j] < array[min])
         {
 		      min = j;
         }
     }
+  if (min != i)
+  {
 	tmp = array[i];
 	array[i] = array[min];
 	array[min] = tmp;
 	print_array(array, size);
+  }
   }
 }
